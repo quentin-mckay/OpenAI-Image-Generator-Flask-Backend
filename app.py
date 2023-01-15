b3ad4ebe9b20c57eb879277986b9e8a23c637a0a
Quentin McKay <quentinmckay1@gmail.com>
2023-01-16 09:52:05 +1100

    initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from config import OPENAI_API_KEY
+import openai
+
+app = Flask(__name__)
+
+openai.api_key = OPENAI_API_KEY
+
+
+# =============================================
+@app.route('/')
+def index(): 
+    return 'hello world'
+
+
+@app.route('/generateimage', methods=['POST'])
+def generate_image():
+
+    prompt = request.json['prompt']
+    size = request.json['size']
+
+    try:
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size=size
+        )
+
+        image_url = response['data'][0]['url']
+
+        return {
+            "success": True,
+            "image_url": image_url
+        }
+
+    except openai.error.OpenAIError as e:
+        print(e.http_status)
+        print(e.error)
+
+        return {
+            "success": False,
+            "error_message": "The image could not be generated."
+        }
+
+
+# =============================================
+if __name__ == '__main__':
+    app.run()
+
+
